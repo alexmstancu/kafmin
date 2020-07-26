42c7a5c04a166a8f829aaf364e6a9f8584127ac3
Alex Stancu <alex.stancu96@outlook.com>
2020-07-26 20:10:34 +0300

    kafka admin skeleton, connection to cluster is working

--- src/main/java/org/kafmin/kafka/KafkaAdministrationCenter.java
+++ src/main/java/org/kafmin/kafka/KafkaAdministrationCenter.java
@@ -5,41 +5,51 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.DescribeClusterResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+@Service
 public class KafkaAdministrationCenter {
+    public static final String HTTP_LOCALHOST_9093 = "http://localhost:9092";
     private final Logger logger = LoggerFactory.getLogger(KafkaAdministrationCenter.class);
 
-    private Map<String, Admin> kafkaAdminClientsByCluster = new HashMap<>();
+    private Map<String, Admin> kafkaAdminByCluster = new HashMap<>();
 
     public KafkaAdministrationCenter() {
-        Admin kafkaAdminClient = createAdmin();
+        Admin kafkaAdminClient = createAdmin(HTTP_LOCALHOST_9093);
+        logger.debug("Starting the KafkaAdministrationCenter");
         DescribeClusterResult describeClusterResult = kafkaAdminClient.describeCluster();
         try {
-            logger.debug("ClusterId {}", describeClusterResult.clusterId().get());
-            logger.debug("Nodes {}", describeClusterResult.nodes().get());
-            logger.debug("Controller {}", describeClusterResult.controller().get());
+            logger.debug("Describing ClusterId: {}, Controller: {}, Nodes: {}.",
+                describeClusterResult.clusterId().get(),
+                describeClusterResult.controller().get(),
+                describeClusterResult.nodes().get());
         } catch (Exception e) {
-            logger.error("Something went wrong describing ");
+            logger.error("Something went wrong describing cluster " + HTTP_LOCALHOST_9093, e);
         }
     }
 
     public DescribeClusterResult describeCluster(String clusterId) throws ExecutionException, InterruptedException {
-        Admin kafkaAdminClient = kafkaAdminClientsByCluster.get(clusterId);
-        DescribeClusterResult clusterResult = kafkaAdminClient.describeCluster();
+        DescribeClusterResult clusterResult = getClusterAdmin(clusterId).describeCluster();
+
         clusterResult.clusterId().get();
         clusterResult.nodes().get();
         clusterResult.controller().get();
+
         return clusterResult;
     }
 
-    public Admin createAdmin() {
+    private Admin getClusterAdmin(String clusterId) {
+        return kafkaAdminByCluster.computeIfAbsent(clusterId, this::createAdmin);
+    }
+
+    public Admin createAdmin(String clusterId) {
         Properties properties = new Properties();
-        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092");
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, clusterId);
         return Admin.create(properties);
     }
 }
