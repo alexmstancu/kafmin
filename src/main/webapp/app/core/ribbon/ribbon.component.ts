d0080e1e11f71d01cd7f8df5c59cbceef6fda701
Alex Stancu <alex.stancu96@outlook.com>
2020-08-09 11:25:43 +0300

    [ci skip] hide the red ribbon saying 'dev'

--- src/main/webapp/app/core/ribbon/ribbon.component.ts
+++ src/main/webapp/app/core/ribbon/ribbon.component.ts
@@ -7,6 +7,7 @@ export default class Ribbon extends Vue {
   }
 
   public get ribbonEnabled(): boolean {
-    return this.$store.getters.ribbonOnProfiles && this.$store.getters.activeProfiles.indexOf(this.$store.getters.ribbonOnProfiles) > -1;
+    // return this.$store.getters.ribbonOnProfiles && this.$store.getters.activeProfiles.indexOf(this.$store.getters.ribbonOnProfiles) > -1;
+    return false; 
   }
 }
