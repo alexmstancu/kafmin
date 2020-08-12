a552e01b469599b624e9f3929df06c35659d9eab
Alex Stancu <alex.stancu96@outlook.com>
2020-08-12 17:27:47 +0300

    [ci skip] added extra fields to message.model.ts

--- src/main/webapp/app/shared/model/message.model.ts
+++ src/main/webapp/app/shared/model/message.model.ts
@@ -3,8 +3,19 @@ export interface IMessage {
   key?: string;
   message?: string;
   partition?: number;
+  topic?: string;
+  date?: Date;
+  offest?: number
 }
 
 export class Message implements IMessage {
-  constructor(public id?: number, public key?: string, public message?: string, public partition?: number) {}
+  constructor(
+    public id?: number,
+    public key?: string,
+    public message?: string,
+    public partition?: number,
+    public topic?: string,
+    public date?: Date,
+    public offest?: number,
+  ) {}
 }
